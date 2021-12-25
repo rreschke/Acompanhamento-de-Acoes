00013bca8d8b761676ab8e86e6068a9fff28a547
Renan Reschke <renanreschke@hotmail.com>
2021-12-25 14:55:11 -0300

    log

--- src/util/log.py
+++ src/util/log.py
@@ -1,7 +1,14 @@
 from datetime import datetime
-
+from os import path
 class Log:
+
+    log: str
+    _root = path.dirname(path.abspath(__file__))
 
     @staticmethod
     def log_date_time(msg: str):
         print(f"{datetime.now()} LOG: {msg}")
+
+    def write_log(self):
+        with open(path.join(self._root, f"""src/logs/{datetime.now().strftime("%Y%m%d[%H:%M:%S")}.txt"""), "w") as f:
+            f.write(self.log)
